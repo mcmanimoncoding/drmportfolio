e8bbedef65ac7c81b730c52cd69737cf85035d25
Daniel M <mcmanimoncoding@gmail.com>
2019-09-10 17:44:44 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//Dumb Components
+// const Index = () => <div>Index</div>
+
+
+// const Index = function(){
+//     return (
+//         <h1>Index Page</h1>
+//     )
+        
+    
+// }
+
+//Class component for lifecycle function - more stuffffffff
+
+class Index extends React.Component{
+
+    render(){
+        return(
+            <h1>Index Page!</h1>
+        )
+    }
+
+}
+
+export default Index
